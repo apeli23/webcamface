54952f7431db4e855d8837e10ac052b0a09c80ab
apeli23 <apelibrian@gmail.com>
2022-10-12 14:22:52 +0300

    full

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -4,7 +4,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  // upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
 });
 
 const config = {
